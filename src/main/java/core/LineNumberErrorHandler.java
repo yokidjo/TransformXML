7f22a776af004339e366cc7d59788adaf1d899b5
yokidjo <simple@>
2019-11-01 11:56:45 +0300

    add

---
+++ src/main/java/core/LineNumberErrorHandler.java
@@ -0,0 +1,26 @@
+package core;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class LineNumberErrorHandler implements ErrorHandler {
+
+    @Override
+    public void warning(SAXParseException exception) throws SAXException {
+        String e = "Line: " + exception.getLineNumber() + " : " + exception.getMessage();
+        throw new SAXException(e);
+    }
+
+    @Override
+    public void error(SAXParseException exception) throws SAXException {
+        String e = "Line: " + exception.getLineNumber() + " : " + exception.getMessage();
+        throw new SAXException(e);
+    }
+
+    @Override
+    public void fatalError(SAXParseException exception) throws SAXException {
+        String e = "Line: " + exception.getLineNumber() + " : " + exception.getMessage();
+        throw new SAXException(e);
+    }
+}
