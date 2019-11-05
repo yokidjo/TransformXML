4afa46aa8018ba4bc3e235ec95d8e06dd63d8200
yokidjo <oleg.patsey@mail.ru>
2019-11-06 02:27:45 +0300

    update

--- src/main/java/core/xml/XmlEngine.java
+++ src/main/java/core/xml/XmlEngine.java
@@ -5,10 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
@@ -16,6 +13,8 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class XmlEngine {
@@ -23,72 +22,88 @@ public class XmlEngine {
     private static Logger logger = LogManager.getRootLogger();
 
     /**
-     * Method transform data file by style sheet
-     *
-     * @param pathXML       is path of data file
-     * @param pathXSL       is patch of stylesheet file
-     * @param pathDirectory is patch of output directory
-     * @return true is success, else if files not exist or get exception
-     * @throws XmlException not found file, transform unsuccessfully
-     */
-    public static boolean transformFile(String pathXML, String pathXSL, String pathDirectory) throws XmlException {
-        File xmlFile = new File(pathXML);
-        File stylesheet = new File(pathXSL);
-        if (!xmlFile.isFile() || !stylesheet.isFile()) {
-            throw new XmlException("Not found file by path");
-        }
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Source xslt = new StreamSource(stylesheet);
-        try {
-            Transformer transformer = factory.newTransformer(xslt);
-            Source xml = new StreamSource(xmlFile);
-            File directory = new File(pathDirectory);
-            if (!directory.exists()) {
-                if (!directory.mkdirs()) {
-                    return false;
-                }
-            }
-            StreamResult streamResult = new StreamResult(
-                    new File(Paths.get(directory.getAbsolutePath(), OUT_FILE).toString())
-            );
-
-            transformer.transform(xml, streamResult);
-            logger.info("Transform file successfully. " + xmlFile.getName());
-            return true;
-        } catch (TransformerException e) {
-            logger.info("Transform file unsuccessfully. " + xmlFile.getName());
-            throw new XmlException("Transform file unsuccessfully. " + xmlFile.getName() +
-                    "\n" + e.getMessage());
-        }
-    }
-
-    /**
      * Method validate data file  by XML schema
      *
      * @param pathXML is path of data file
      * @param pathXSD is path of XML schema
-     * @return true is success, else if files not exist or get exception
+     * @throws XmlException an error occurred while parsing XML ,an error occurred while while work with file
      */
-    public static boolean validateFIle(String pathXML, String pathXSD) {
-        File xmlFile = new File(pathXML);
-        File xsdFile = new File(pathXSD);
-        if (!xmlFile.isFile() || !xsdFile.isFile()) {
-            logger.info("Not found file by path");
-            return false;
-        }
-        Source xmlSource = new StreamSource(xmlFile);
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    public static void validateFIle(Path pathXML, Path pathXSD) throws XmlException {
+        File xmlFile = new File(pathXML.toAbsolutePath().toString());
+        File xsdFile = new File(pathXSD.toAbsolutePath().toString());
+        existFiles(xmlFile, xsdFile);
         try {
+            Source xmlSource = new StreamSource(xmlFile);
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = schemaFactory.newSchema(xsdFile);
             Validator validator = schema.newValidator();
             validator.validate(xmlSource);
             validator.setErrorHandler(new LineNumberErrorHandler());
             logger.info("Check file successfully. " + xmlFile.getName());
-            return true;
-        } catch (SAXException | IOException e) {
-            logger.info("Check file unsuccessfully. " + xmlFile.getName());
-            logger.error(e.getMessage());
-            return false;
+        } catch (SAXException e) {
+            throw new XmlException("Check file unsuccessfully. An error occurred while parsing XML. " + xmlFile.getName() + " " + e.getMessage());
+        } catch (IOException e) {
+            throw new XmlException("Check file unsuccessfully. An error occurred while while work with file . " + xmlFile.getName() + " " + e.getMessage());
+        }
+    }
+
+    /**
+     * Method transform data file by style sheet
+     *
+     * @param pathXML       is path of data file
+     * @param pathXSL       is patch of stylesheet file
+     * @param pathDirectory is patch of output directory
+     * @throws XmlException not found file, transform unsuccessfully
+     */
+    public static void transformFile(Path pathXML, Path pathXSL, Path pathDirectory) throws XmlException {
+        File xmlFile = new File(pathXML.toAbsolutePath().toString());
+        File stylesheet = new File(pathXSL.toAbsolutePath().toString());
+        existFiles(xmlFile, stylesheet);
+        try {
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(stylesheet));
+            Source xmlSource = new StreamSource(xmlFile);
+            createDir(pathDirectory);
+            StreamResult streamResult = new StreamResult(
+                    new File(
+                            Paths.get(pathDirectory.toAbsolutePath().toString(), OUT_FILE).toString()
+                    )
+            );
+            transformer.transform(xmlSource, streamResult);
+            logger.info("Transform file successfully. " + xmlFile.getName());
+        } catch (TransformerConfigurationException e) {
+            throw new XmlException("Transform file unsuccessfully. Not create factory transform. " + "\n" + e.getMessage());
+        } catch (TransformerException e) {
+            throw new XmlException("Transform file unsuccessfully. Transform file unsuccessfully. " + xmlFile.getName() +
+                    "\n" + e.getMessage());
+        } catch (IOException e) {
+            throw new XmlException("Transform file unsuccessfully. Can not create directory. " + e.getMessage());
+        }
+
+    }
+
+    /**
+     * Method create directory by path
+     *
+     * @param pathDirectory is directory to  create
+     * @throws IOException occurrence of a problem during directory creation
+     */
+    private static void createDir(Path pathDirectory) throws IOException {
+        if (!Files.exists(pathDirectory)) {
+            Files.createDirectories(pathDirectory);
+        }
+    }
+
+    /**
+     * Method check exist files
+     *
+     * @param xmlFile    xml file
+     * @param stylesheet xsl file
+     * @throws XmlException path not file
+     */
+    private static void existFiles(File xmlFile, File stylesheet) throws XmlException {
+        if (!xmlFile.isFile() || !stylesheet.isFile()) {
+            throw new XmlException("Not found file by path");
         }
     }
 }
